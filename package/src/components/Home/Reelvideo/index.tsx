bdcb0fade81857d0c21e8aba2b62410d7979da22
Julee2511 <julee6511@gmail.com>
2024-09-24 18:27:47 +0530

    "

--- package/src/components/Home/Reelvideo/index.tsx
+++ package/src/components/Home/Reelvideo/index.tsx
@@ -13,7 +13,7 @@ const Reelvideo = () => {
                 <div className="md:col-span-7">
                         <Image
                             src="/images/Reelvideo/reel.png"
-                            alt="Work Progress"
+                            alt="reel"
                             width={575}
                             height={0}
                             quality={100}
@@ -39,40 +39,51 @@ const Reelvideo = () => {
                     </div>
                 </div>
 
-                <div className="grid md:grid-cols-12 gap-7 mt-8">
-                <div className="md:col-span-4">
-                <div className="flex  items-center mt-16">  
-                            <p className="font-medium text-primary text-14 dark:text-white dark:text-opacity-50 pb-5">
-                            FEATURES
-                            </p>
-                        </div>
-                
-                        <p className="text-grey dark:text-white dark:text-opacity-70 text-16 mr-4 mb-6">
-                        Long run, and work as an extension of the merchant's team.
-                        </p>
-                        
-            <button className="hidden lg:block bg-transparent border-2 border-black text-black px-4 py-2 rounded-full hover:bg-black hover:text-white">Read More
-            </button>
-                    </div>
-                 
-                    <div className="md:col-span-8">
-                    <div className="flex">
-            <div className="flex border border-blue-200 rounded-lg shadow-sm mr-20">
-                <div className="p-6 border-r border-blue-200">
-                    <div className="flex items-center justify-center w-12 h-12 bg-blue-500 rounded-full text-white mb-4">
-                        <i className="fas fa-pencil-alt"></i>
-                    </div>
-                    <h3 className="text-gray-800 text-xl font-semibold">Professional</h3>
-                    <p className="text-gray-600 mt-2 mr-4">Full service range including technical skills, design.</p>
-                </div>
-                <div className="p-3 flex flex-col justify-center mt-3">
-                    <h3 className="text-gray-800 text-xl font-semibold">Accessibility</h3>
-                    <p className="text-gray-600 mt-2">Business understanding, ability to put themselves.</p>
-                </div>
-            </div>
+                <div className="grid md:grid-cols-12  mt-8">
+  <div className="md:col-span-4">
+    <div className="flex items-center mt-24">  
+      <p className="font-medium text-primary text-[14px] dark:text-white dark:text-opacity-50 pb-5">
+        FEATURES
+      </p>
+    </div>
+    
+    <p className="text-gray-600 dark:text-white dark:text-opacity-70  text-20 mb-6">
+      Long run, and work as an extension of the merchant's team.
+    </p>
+    
+    <button className="hidden lg:block bg-transparent border-2 border-black text-black px-4 py-2 rounded-full hover:bg-black hover:text-white">
+      Read More
+    </button>
+  </div>
+  
+  <div className="md:col-span-8 mt-4">
+    <div className="flex justify-center items-center">
+      <div className="flex">
+        <div className="border border-blue-500 rounded-tl-lg rounded-bl-lg  rounded-tr-lg  flex flex-col items-start bg-white  h-72 w-72 p-6">
+          <div className="flex items-center justify-center w-12 h-12 mb-8 mt-9">
+          <Image
+                            src="/images/Reelvideo/cardicon.svg"
+                            alt="cardicon"
+                            width={575}
+                            height={0}
+                            quality={100}
+                            style={{ width: '100%', height: '100%' }}
+                            className="md:block hidden"
+                        />
           </div>
+          <h2 className="text-xl font-semibold text-gray-900 mb-5">Professional</h2>
+          <p className="text-gray-600 ">Full service range including technical skills, design.</p>
         </div>
-       </div>
+        
+        <div className="border border-blue-500 border-l-0 rounded-tr-lg rounded-br-lg p-6 flex flex-col items-start bg-white h-auto mt-12 w-72">
+          <h2 className="text-xl font-semibold text-gray-900 mb-6 mt-16">Accessibility</h2>
+          <p className="text-gray-600">Business understanding, ability to put themselves.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
         </div>  
             </section>
             
